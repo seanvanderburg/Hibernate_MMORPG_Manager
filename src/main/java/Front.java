fadc15b7e9760c39f0a2ff1e81f7377bdccad1fb
Sean van der Burg <seanvanderburg@msn.com>
2015-10-13 16:45:30 +0200

    removed some lines

--- src/main/java/Front.java
+++ src/main/java/Front.java
@@ -1,9 +1,11 @@
+import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
--- src/main/java/entities/Character.java
+++ src/main/java/entities/Character.java
@@ -1,17 +1,12 @@
 package entities;
 
 
-import java.util.HashSet;
-import java.util.Set;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
 @Entity
